040499ed8c9e70812651a3359a91105fe4e643a1
Yahya <yahia@bazooka.se>
2016-03-24 11:12:08 +0100

    Car detail working (but exposed to sql injections...)

--- details.php
+++ details.php
@@ -1,3 +1,13 @@
+<?php 
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="carsworld";
+
+$conn= new mysqli($servername,$username,$password,$dbname);
+
+
+?>
 <html>
 <head>
 <meta http-equiv="content-type" charset="UTF-8"/>
@@ -5,9 +15,18 @@
 </head>
 <body>
 
-Details of a car.
+<?php
 
-Email field and buy button
+$carid=$_GET["id"];
+$query = "SELECT * FROM `cars` WHERE `Id`= " . $carid;
+echo $query;
+$result = $conn->query($query);
 
+if ($result){
+	$row=$result->fetch_assoc();
+	echo $row["Name"];	
+}
+	
+?>	
 </body>
 </html>
--- list.php
+++ list.php
@@ -6,7 +6,6 @@ $dbname="carsworld";
 
 $conn= new mysqli($servername,$username,$password,$dbname);
 $cars = array ("volvo","bmw","saap");
-$mychoice=$_POST["mychoice"];
 ?>
 <html>
 <head>
@@ -20,21 +19,18 @@ $result=$conn->query("SELECT * FROM `cars`");
 if ($result)
 {
 	while ($row=$result->fetch_assoc()){
-?>
-	<div>
+ ?>
+	 <div>
 		<h2><?php echo $row["Name"] ?></h2>
 		<p>Färg: <?php echo $row["Color"] ?></p>
 		<p>Pris: <?php echo $row["Price"] ?></p>
 		<a href="details.php?id=<?php echo $row["Id"] ?>">Köp</a>
-	</div>
-<?php
+	 </div>
+     <?php  
 	}
 }
 ?>
 
-<button type="submit" name="mychoice" value="volvo" >buy</button>
-<button type="submit" name="mychoice" value="bmw">buy</button>
-<button type="submit" name="mychoice" value="saap">buy</button>
 
 </body>
 </html>
