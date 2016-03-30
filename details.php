9750982f044597327db38c0483bdb16de8eb8edd
Yahya <yahia@bazooka.se>
2016-03-30 14:53:13 +0200

    check email form not working

--- details.php
+++ details.php
@@ -16,27 +16,30 @@ $conn= new mysqli($servername,$username,$password,$dbname);
 <body>
 
 <?php
-
-$carid=$_GET["id"];
-
 $stmt = $conn->prepare("SELECT * FROM `cars` WHERE Id = ?");
-$stmt->bind_param('i', $carid);
+$stmt->bind_param('i', $Carid);
 $stmt->execute();
 $result = $stmt->get_result();
 
 if ($result){
 	$row=$result->fetch_assoc();
-	echo $row["Name"];	
+	echo $row["Name"].$row["Color"].$row["Number"].$row["Price"];
 }
+	
+
+$Email = "yahia@bazooka.com";
+
+if (!filter_var($Email, FILTER_VALIDATE_EMAIL) === true) {
+  echo("Sorry, we don't recognize this email.");
+} 
+
 	
 ?>
 
 <form method="GET" action="order.php">
-	<input type="text" name="mail"/>
+	<input type="text" name="Email"/>
 	<button >Submit</button>
-	<input type="hidden" name="carid" value="<?php echo $carid ?>"/>
-	
-	</form>
-
+	<input type="hidden" name="Carid" value="<?php echo $Carid ?>"/>
+</form>
 </body>
 </html>
